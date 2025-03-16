0c2edac26b41045342a8c5ce0795cb806196ff25
spark00001 <s20512165@gmail.com>
2025-03-16 23:00:10 +0530

    Create pay.js

---
+++ pay.js
@@ -0,0 +1,9 @@
+<div id='frame'>
+<h4>JOIN</h4>
+<h4>MEMBERSHIP</h4>
+<hr>
+<embed src='/images/logo.png' style='width:100px;height:80px;border-radius:30px;'></embed>
+<h2><b>Rs.99</b></h2>
+<p>PER MONTH</p>
+<button style='background-image: linear-gradient(-225deg, #FF3CAC 0%, #562B7C 52%, #2B86C5 100%);color:gold;font-size:21px;border:1px solid black;padding:10px 100px;border-radius:20px;'>PAY</button>
+</div>
