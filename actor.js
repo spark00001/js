e218eff44b9739df40bd4925b8d53bb19d38196a
spark00001 <s20512165@gmail.com>
2025-05-10 21:35:32 +0530

    Add files via upload

---
+++ actor.js
@@ -0,0 +1,31 @@
+<!-- see more-->
+function seeall(){
+location.assign("/spark/404pagenotfound.html");
+}
+<!--actors-->
+
+function sk(){
+location.assign("/spark/404pagenotfound.html");
+}
+function srk(){
+location.assign("/spark/404pagenotfound.html");
+}
+function srk(){
+location.assign("/spark/404pagenotfound.html");
+}
+
+function amir_khan(){
+location.assign("/spark/404pagenotfound.html");
+}
+function aa(){
+location.assign("/spark/404pagenotfound.html");
+}
+function ntr(){
+location.assign("/spark/404pagenotfound.html");
+}
+function ravi_teja(){
+location.assign("/spark/404pagenotfound.html");
+}
+function ram_charan(){
+location.assign("/spark/404pagenotfound.html");
+}
