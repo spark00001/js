3b12f0c3237191317a2ce674a42a77329d5063d7
spark00001 <s20512165@gmail.com>
2025-05-16 18:11:20 +0530

    Add files via upload

---
+++ language.js
@@ -0,0 +1,24 @@
+<!-- see more-->
+function seeall(){
+location.assign("/spark/actor.html");
+}
+<!--actors-->
+
+  function punjabi(){
+location.assign("/spark/punjabi.html");
+}
+function south(){
+location.assign("/spark/404pagenotfound.html");
+}
+function bolllywood(){
+location.assign("/spark/404pagenotfound.html");
+}
+function marathi(){
+location.assign("/spark/404pagenotfound.html");
+}
+function gujarati(){
+location.assign("/spark/404pagenotfound.html");
+}
+function bengali(){
+location.assign("/spark/404pagenotfound.html");
+}
