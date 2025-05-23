33f2c0dd8ce3304abe6e93502135083b46863e3b
spark00001 <s20512165@gmail.com>
2025-05-24 00:29:41 +0530

    Add files via upload

---
+++ livetv.js
@@ -0,0 +1,33 @@
+function abpganga(){
+location.assign("/spark/abpganga.html");
+}
+function ninexjhakaas(){
+location.assign("/spark/ninexjhakaas.html");
+}
+function ninextashan(){
+location.assign("/spark/ninextashan.html");
+}
+function ninexm(){
+location.assign("/spark/ninexm.html");
+}
+function abpmajha(){
+location.assign("/spark/abpmajha.html");
+}
+function animax(){
+location.assign("/spark/animax.html");
+}
+function anjan(){
+location.assign("/spark/anjan.html");
+}
+function annnews(){
+location.assign("/spark/annnews.html");
+}
+function aajtak(){
+location.assign("/spark/aajtak.html");
+}
+function aasthabhajan(){
+location.assign("/spark/aasthabhajan.html");
+}
+function asianetnews(){
+location.assign("/spark/asianetnews.html");
+}
