bb55458a2c2f3d468a6e98c10bee5f9724e75d23
spark00001 <s20512165@gmail.com>
2025-05-24 12:39:10 +0530

    Update ind.js

--- ind.js
+++ ind.js
@@ -1,6 +1,6 @@
 document.getElementById("ind").innerHTML="<div style='border:1px solid orange;border-radius:20px;color:gold;height:90px;width:99%;padding:;'>         <img src='/actor/ind.png' style='border:1px solid white; border-radius:50%;height:90px;width:90px;' onclick='indlivetv()'><b style='position:relative;top:-40px;left:50px;outline:1px solid red;border-radius:20px;border:1px solid gold;padding:10px;' onclick='indlivetv()'>INDIAN TV</b>         <img style='position:relative;top:-20px;left:150px;outline:1px solid red;border-radius:50%;border:1px solid gold;padding:px;width:50px;height:50px;' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvU1a509pduQk_sWPAkkryIsQS7TAHc5kdxQ&usqp=CAU' onclick='srch()'>";
  document.getElementById("pnjb").innerHTML="<div style='border:1px solid orange;border-radius:20px;color:gold;height:90px;width:99%;padding:;'>         <img src='/actor/punjabi.jpg' style='border:1px solid white; border-radius:50%;height:90px;width:90px;' onclick='punjabi()'><b style='position:relative;top:-40px;left:50px;outline:1px solid red;border-radius:20px;border:1px solid gold;padding:10px;' onclick='punjabi()'>PUNJABI</b>         <img style='position:relative;top:-20px;left:150px;outline:1px solid red;border-radius:50%;border:1px solid gold;padding:px;width:50px;height:50px;' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvU1a509pduQk_sWPAkkryIsQS7TAHc5kdxQ&usqp=CAU' onclick='srch()'>";
-function indianlivetv(){
+function indlivetv(){
         location.assign("/spark/indianlivetv.html");
     }
 function punjabi(){
