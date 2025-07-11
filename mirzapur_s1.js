54ec3923e6aff96f61c8d72d1bf856ae4c3363ef
spark00001 <s20512165@gmail.com>
2025-07-11 13:15:55 +0530

    Update mirzapur_s1.js

--- mirzapur_s1.js
+++ mirzapur_s1.js
@@ -4,6 +4,7 @@ function mpur(){
 var s1 = document.getElementById("mpur").value;
 if(s1 == "s1ep1"){
 document.getElementById("mirzapur").innerHTML="<video id='hls-example' class='video-js vjs-default-skin' width='400' height='300' controls style='width:100%;' poster='https://spark00001.github.io/images/mirzapur.png'> <source style='width:100%;' src='https://pixeldrain.dev/api/file/E8NJeTTj?download' type='application/x-mpegURL'/></video><br>MIRZAPUR SEASON 1 EPISODE 1<hr>";
+document.getElementById("mpur").style.top="315px";
 }
 if(s1 == "s1ep2"){
 document.getElementById("mirzapur").innerHTML="<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'><iframe src='https://drive.google.com/file/d/1fklMk9SW8Bs6u0qswFn2ToLDqAKV61X5/preview' style='top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;' allowfullscreen scrolling='no' allow='encrypted-media *;'></iframe></div><br>MIRZAPUR SEASON 1 EPISODE 2<hr>";
@@ -11,28 +12,31 @@ document.getElementById("mpur").style.top="250px";
 }
 if(s1 == "s1ep3"){
 document.getElementById("mirzapur").innerHTML="<video id='hls-example' class='video-js vjs-default-skin' width='400' height='300' controls style='width:100%;' poster='https://spark00001.github.io/images/mirzapur.png'> <source style='width:100%;' src='https://pixeldrain.dev/api/file/rJV7DKau?download' type='application/x-mpegURL'/></video><br>MIRZAPUR SEASON 1 EPISODE 3<hr>";
+document.getElementById("mpur").style.top="315px";
 }
 if(s1 == "s1ep4"){
 document.getElementById("mirzapur").innerHTML="<video id='hls-example' class='video-js vjs-default-skin' width='400' height='300' controls style='width:100%;' poster='https://spark00001.github.io/images/mirzapur.png'> <source style='width:100%;' src='https://pixeldrain.dev/api/file/bGzxEJvs?download' type='application/x-mpegURL'/></video><br>MIRZAPUR SEASON 1 EPISODE 4<hr>";
+document.getElementById("mpur").style.top="315px";
 }
 if(s1 == "s1ep5"){
 document.getElementById("mirzapur").innerHTML="<video id='hls-example' class='video-js vjs-default-skin' width='400' height='300' controls style='width:100%;' poster='https://spark00001.github.io/images/mirzapur.png'> <source style='width:100%;' src='https://pixeldrain.dev/api/file/62tBs18w?download' type='application/x-mpegURL'/></video><br>MIRZAPUR SEASON 1 EPISODE 5<hr>";
+document.getElementById("mpur").style.top="315px";
 }
 if(s1 == "s1ep6"){
 document.getElementById("mirzapur").innerHTML="<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'><iframe src='https://drive.google.com/file/d/1fu3LGUmcBCqhxoWdWoze7mESbI3NQf9X/preview' style='top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;' allowfullscreen scrolling='no' allow='encrypted-media *;'></iframe></div><br>MIRZAPUR SEASON 1 EPISODE 6<hr>";
-  document.getElementById("mpur").style.top="250px";
+document.getElementById("mpur").style.top="250px";
 }
 if(s1 == "s1ep7"){
 document.getElementById("mirzapur").innerHTML="<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'><iframe src='https://drive.google.com/file/d/1ftIPaT0Iewr3Ooa51kpLKCmgfaVDK4Ps/preview' style='top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;' allowfullscreen scrolling='no' allow='encrypted-media *;'></iframe></div><br>MIRZAPUR SEASON 1 EPISODE 7<hr>";
-  document.getElementById("mpur").style.top="250px";
+document.getElementById("mpur").style.top="250px";
 }
 if(s1 == "s1ep8"){
 document.getElementById("mirzapur").innerHTML="<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'><iframe src='https://drive.google.com/file/d/1fmovI7RRuB0XvVkwON_wEucxTY5u4K_J/preview' style='top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;' allowfullscreen scrolling='no' allow='encrypted-media *;'></iframe></div><br>MIRZAPUR SEASON 1 EPISODE 8<hr>";
-  document.getElementById("mpur").style.top="250px";
+document.getElementById("mpur").style.top="250px";
 }
 if(s1 == "s1ep9"){
 document.getElementById("mirzapur").innerHTML="<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;'><iframe src='https://drive.google.com/file/d/1gCAbgdfaSYJwi-_jFh7EPafQy5b1ecyr/preview' style='top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;' allowfullscreen scrolling='no' allow='encrypted-media *;'></iframe></div><br>MIRZAPUR SEASON 1 EPISODE 9<hr>";
-  document.getElementById("mpur").style.top="250px";
+document.getElementById("mpur").style.top="250px";
 }
 
 }
