6e71f6453687f14e67c15f074fabd8e31939da84
spark00001 <s20512165@gmail.com>
2025-07-15 23:35:59 +0530

    Create search_retrieve.js

---
+++ search_retrieve.js
@@ -0,0 +1,67 @@
+function findout(){
+var x = document.getElementById("find").value;
+/* if(x.toLowerCase() == ""){
+        const  = setTimeout(,100);
+        }   */
+
+<!--head over heels-->
+
+if(x.toLowerCase() == "head over heels"){
+        const head_over_heels_s1 = setTimeout(head_over_heels,100);
+        }  
+if(x.toLowerCase() == "head over heels season 1"){
+        const head_over_heels_s1 = setTimeout(head_over_heels,100);
+        }  
+if(x.toLowerCase() == "head over heels season1"){
+        const head_over_heels_s1 = setTimeout(head_over_heels,100);
+        }  
+
+
+<!-- good boy-->
+
+if(x.toLowerCase() == "good boy"){
+        const good_boy_s1 = setTimeout(good_boy,100);
+        }
+if(x.toLowerCase() == "good boy season 1"){
+        const good_boy_s1 = setTimeout(good_boy,100);
+        }
+if(x.toLowerCase() == "good boy season1"){
+        const good_boy_s1 = setTimeout(good_boy,100);
+        }
+
+<!-- live telecast-->
+
+if(x.toLowerCase() == "live telecast"){
+const live_telecast_s1 = setTimeout(live_telecast,100);
+}
+if(x.toLowerCase() == "live telecast season 1"){
+const live_telecast_s1 = setTimeout(live_telecast,100);
+}
+if(x.toLowerCase() == "live telecast season1"){
+const live_telecast_s1 = setTimeout(live_telecast,100);
+}
+
+<!--she-->
+
+if(x.toLowerCase() == "she"){
+const she_s1 = setTimeout(she,100);
+}
+if(x.toLowerCase() == "she season 1"){
+const she_s1 = setTimeout(she,100);
+}
+if(x.toLowerCase() == "she season1"){
+const she_s1 = setTimeout(she,100);
+}
+
+<!-- campus diaries-->
+
+if(x.toLowerCase() == "campus diaries"){
+const campus_diaries_s1 = setTimeout(campus_diaries,100);
+}
+if(x.toLowerCase() == "campus diaries season 1"){
+const campus_diaries_s1 = setTimeout(campus_diaries,100);
+}
+if(x.toLowerCase() == "campus diaries season1"){
+const campus_diaries_s1 = setTimeout(campus_diaries,100);
+}
+}
