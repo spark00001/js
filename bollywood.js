f5a7214067793b31feb7eadceec935ba683017f9
spark00001 <s20512165@gmail.com>
2025-07-18 15:06:47 +0530

    Update bollywood.js

--- bollywood.js
+++ bollywood.js
@@ -1,2 +1,6 @@
+function bollywood(){
+document.getElementById("bollywoodmain").innerHTML="<p id='bollywood'></p>";
+}
+
 document.getElementById("bollywood").innerHTML="<div id='gallery' class='gallery'>    <!--2025-->	                                                                      <div class='image-item'>            <img src='/images/jaat_poster.png' alt='jaat' onclick='jaat();'> <p>JAAT <BR>(2025)</p>        </div>	                       <div class='image-item'>            <img src='/images/sikandar_poster.png' alt='sikandar' onclick='sikandar();'> <p>SIKANDAR<br>(2025)</p>   </div>   </div>";
    
