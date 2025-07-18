5c1a4195100597f142052ed964a3e906058a39d6
spark00001 <s20512165@gmail.com>
2025-07-18 21:43:45 +0530

    Update genere.js

--- genere.js
+++ genere.js
@@ -32,7 +32,8 @@ document.getElementById("webseries").style.display="none";
 }
 if(lang == "bollywood"){
 document.getElementById("all").innerHTML="<b style='color:gold;width:99%;height:60px;border:1px solid gold;padding:10px;'>BOLLYWOOD</b><br><br><p id='home'></p><br><p id='webseries'></p><br><p id='south'></p><br><p id='bollywood'></p><br><p id='hollywood'></p>";
-
+const bollywood = setaTimeout(bollywood,100)
+;
 document.getElementById("home").style.display="none";
 document.getElementById("south").style.display="none";
 document.getElementById("hollywood").style.display="none";
