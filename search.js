d8f51b68bdd9e5366a164f5a60f8e684e3ff8b60
spark00001 <s20512165@gmail.com>
2025-08-12 08:37:11 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,15 +1,11 @@
-function srch(){
-var  inpt = document.getElementById("sr").value;
-if(inpt.toLowerCase() == "sonic 2"){
-document.getElementById("srout").innerHTML="<table><tr><td><embed src='/images/movie-1.png' width='150'height='200' onclick='sonic2();'></embed></td></tr><tr><td>SONIC 2</td></tr></table>";
-}
-if(inpt.toLowerCase() == "sonic"){
-document.getElementById("srout").innerHTML="<table><tr><td><embed src='/images/movie-1.png' width='150'height='200' onclick='sonic2();'></embed></td></tr><tr><td>SONIC 2</td></tr></table>";
-}
-if(inpt.toLowerCase() == "free guy"){
-document.getElementById("srout").innerHTML="<table><tr><td><embed src='/images/movie-4.png' width='150'height='200'></embed></td></tr><tr><td>SONIC 2</td></tr></table>";
-}
-}
-function sonic2(){
-location.assign("/spark/sonic2.html");
-}
+function searches(){
+	    var inputs = document.getElementById("takeinpr").value;
+	   if(inputs == "campus diaries"){
+	   document.getElementById("srchs").innerHTML="<p id='loading'></p>";
+	   const loading = setTimeout(ld,0000);
+	    const view = setTimeout(campusdiaries,000);
+	    }
+	    if(inputs == ""){
+	    const view = setTimeout(reset,0000);
+	    }
+ }
