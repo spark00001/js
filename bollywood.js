98e9d5b83af6dc62966daa215bf9ec90f3c73899
spark00001 <s20512165@gmail.com>
2025-08-13 07:04:27 +0530

    Update bollywood.js

--- bollywood.js
+++ bollywood.js
@@ -2,5 +2,5 @@ function bollywood(){
 document.getElementById("bollywoodmain").innerHTML="<p id='bollywood'></p>";
 }
 
-document.getElementById("bollywood").innerHTML="<div id='gallery' class='gallery'>    <!--2025-->	      <div class='image-item'>            <img src='/bollywood/zora.png' alt='zora' onclick='zora();'> <p>ZORA (2025)</p>        </div>                                                                    <div class='image-item'>            <img src='/images/jaat_poster.png' alt='jaat' onclick='jaat();'> <p>JAAT <BR>(2025)</p>        </div>	                       <div class='image-item'>            <img src='/images/sikandar_poster.png' alt='sikandar' onclick='sikandar();'> <p>SIKANDAR<br>(2025)</p>   </div>   </div>";
+document.getElementById("bollywood").innerHTML="<div id='gallery' class='gallery'>    <!--2025-->	      <div class='image-item'>            <img src='/bollywood-/zora.png' alt='zora' onclick='zora();'> <p>ZORA (2025)</p>        </div>                                                                    <div class='image-item'>            <img src='/images/jaat_poster.png' alt='jaat' onclick='jaat();'> <p>JAAT <BR>(2025)</p>        </div>	                       <div class='image-item'>            <img src='/images/sikandar_poster.png' alt='sikandar' onclick='sikandar();'> <p>SIKANDAR<br>(2025)</p>   </div>   </div>";
    
