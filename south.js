173db8e853c96d54a7adc81cf01bbeb27195f64a
spark00001 <s20512165@gmail.com>
2025-08-17 00:24:27 +0530

    Update south.js

--- south.js
+++ south.js
@@ -1,3 +1,3 @@
-function south(){
+
 document.getElementById("south").innerHTML="<div id='gallery' class='gallery'>    <!--2025-->	                                                                     <div class='image-item'>            <img src='/south/pechi.png' alt='jaat' onclick='pechi();' class='effect'> <p>PECHI <BR>(2024)</p>        </div>	                               <div class='image-item'>            <img src='/south/single.png' alt='single' onclick='single();' class='effect'> <p>SINGLE <BR>(2025)</p>        </div>	                                                    <div class='image-item'>            <img src='/south/kgf2.png' alt='KGF 1' onclick='kgf2();' class='effect'> <p>KGF 2<br>(2022)</p></div>                                                           <div class='image-item'>            <img src='/south/kgf1.png' alt='KGF 1' onclick='kgf1();' class='effect'> <p>KGF 1<br>(2018)</p>   </div> </div>";
-}
+
