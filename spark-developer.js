e6d4ff8d60ea14192c979093b038d8b7725b15a6
spark00001 <s20512165@gmail.com>
2025-09-17 19:43:07 +0530

    Add files via upload

---
+++ spark-developer.js
@@ -0,0 +1,32 @@
+const d1 = setTimeout(programmed,100);
+function programmed(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br>";
+const d2 = setTimeout(name,2000);
+}
+function name(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>S</b>";
+const d3 = setTimeout(name2,1000);
+}
+function name2(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>SM</B>";
+const d3 = setTimeout(name3,1000);
+}
+function name3(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>SMA</B>";
+const d4 = setTimeout(name4,1000);
+}
+function name4(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>SMAR</B>";
+const d5 = setTimeout(name5,1000);
+}
+function name5(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>SMARA</B>";
+const d6 = setTimeout(name6,1000);
+}
+function name6(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>SMARAK</B>";
+const d7 = setTimeout(name7,1000);
+}
+function name7(){
+document.getElementById("developer").innerHTML="<u><i style='color:gold;font-size:;'>MADE  WITH &#129505; BY::</i></u><br><b>SMARAK</B>";
+const d1 = setTimeout(programmed,3000);}
