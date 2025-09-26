c62db58d75142814bab1dcbe6c9ce4edc6046a42
spark00001 <s20512165@gmail.com>
2025-09-26 19:40:28 +0530

    Create audio_content.js

---
+++ audio_content.js
@@ -0,0 +1,5 @@
+functiom music(){
+      document.getElementById("songsrch").innerHTML="<br><input id='inputaudio' onkeyup='songs()' type='text' placeholder='Search songs...'><hr><br>";
+     document.getElementById("audio").innerHTML="<div id='song' id='play1'><table><tr><td><p id='titleimg' onclick='ayekhuda()'><img src='https://i.ytimg.com/vi/v0biEkJ9Uv8/sddefault.jpg' class='music'></p></td><td><p id='mtitle' onclick='ayekhuda()'>AYE KHUDA</p></td><td><p id='ayekhuda_button'></p></td><td onclick='d_ayekhuda()'><i class='fa fa-download' style='font-size:px;background-color:gold;'></i>DOWNLOAD</td></tr></table></div><br><div id='song' id='terimeripremkahani'><table><tr><td><p id='titleimg'  onclick='terimeripremkahani()'><img src='https://i1.sndcdn.com/artworks-000105449528-hgc39r-t500x500.jpg' class='music'></p></td><td><p id='mtitle'  onclick='terimeripremkahani()'>TERI MERI PREM KAHANI</p></td><td><p id='terimeripremkahani_button'></p></td><td onclick='d_terimeripremkahani()'><i class='fa fa-download' style='font-size:px;background-color:gold;'></i>DOWNLOAD</td></tr></table></div>";
+     document.getElementById("mainhub").style.display="none";
+}
