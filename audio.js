ff17581d8e004e7cc5e8753d0ee3824f2053e307
spark00001 <s20512165@gmail.com>
2025-09-26 21:10:08 +0530

    Update and rename audio.js to audio_database.js

--- audio.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("spark").innerHTML="<table style=' width:100%;padding: 10px 120px;border:1px solid gold;'><tr><td style='width:auto;padding:10px;' onclick='spark()'>HOME</td><td></td><td style='width:auto;padding:10px;' onclick='music()'>AUDIO</td></tr></table>";
-    function spark(){
-      location.reload();
-    }
-        
