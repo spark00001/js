526453c3a0a1f119026a8e9ca1505c7b839e1ecf
spark00001 <s20512165@gmail.com>
2025-09-30 22:02:30 +0530

    Create pakdrama.js

---
+++ pakdrama.js
@@ -0,0 +1,1 @@
+document.getElementById("pakdrama").innerHTML="<div id='gallery' class='gallery'>  <div class='image-item'>            <img src='/pakdrama/kabhimainkabhitum.png' alt='kabhi main kabhi tum' onclick='kabhimainkabhitum();'> <p>KABHI MAIN <br> KABHI TUM<BR>(2024)</p>        </div>   </div>";
