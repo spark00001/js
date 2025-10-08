363acf93550764c4e148eeb134ca412134343bb1
spark00001 <s20512165@gmail.com>
2025-10-09 00:11:20 +0530

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -1,4 +1,4 @@
-document.getElementById("maintenance").innerHTML="<div style='height:60px;width:99%;border:1px solid red;background-color:gold;color:black;border-radius:10px 40px;'><p style='font-size:25px; position:fixed;left:15px;top:0px;'>&#9432;</p></div><p style='position:fixed;top:10px;left:60px;'>!SPARK IS UNDER MAINTENANCE !<br><p style='position:fixed;top:10px;left:60px;'><marquee>THIS SITE IS INDER MAINTENANCE ALL REMAINING AVAIL SOON</marquee></p>";
+document.getElementById("maintenance").innerHTML="<div style='height:60px;width:99%;border:1px solid red;background-color:gold;color:black;border-radius:10px 40px;'><p style='font-size:25px; position:fixed;left:15px;top:0px;'>&#9432;</p></div><p style='position:fixed;top:10px;left:60px;'>!SPARK IS UNDER MAINTENANCE !<br><p style='position:fixed;top:20px;left:60px;'><marquee>THIS SITE IS INDER MAINTENANCE ALL REMAINING AVAIL SOON</marquee></p>";
 document.getElementById("mainhub").style.display="none";
 document.getElementById("songsrch").style.display="none";
 document.getElementById("audio").style.display="none";
