3e9a60bb5df3d417784707ece5f09455e10e773a
SPARK OFFICIAL <s20512165@gmail.com>
2025-11-02 12:46:58 +0530

    Create songdb.js

---
+++ songdb.js
@@ -0,0 +1,13 @@
+function ayekhuda(){
+            location.assign("/audio/ayekhuda.html");
+        }
+        function terimeripremkahani(){
+            location.assign("/audio/terimeripremkahani.html");
+        }
+		function d_ayekhuda(){
+			location.assign("https://github.com/spark00001/spark/raw/refs/heads/main/Murder%202%20Aye%20Khuda%20Video%20With%20Lyrics%20Emraan%20Hashmi,%20Jacqueline%20Fernandez.mp3");
+		}
+		function d_terimeripremkahani(){
+			location.assign("https://github.com/spark00001/hindi-music/raw/refs/heads/main/Teri%20Meri.mp3");
+		}
+    
