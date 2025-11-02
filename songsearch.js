3c3904ba95fb166e200f84615ecfdca4e358771e
SPARK OFFICIAL <s20512165@gmail.com>
2025-11-02 13:04:14 +0530

    Update songsearch.js

--- songsearch.js
+++ songsearch.js
@@ -10,24 +10,16 @@
     }
         function songs(){
         var takesong = document.getElementById("inputaudio").value;
-        if(takesong.toLowerCase() == "aye khuda" || takesong.toLowerCase() == "a" || takesong.toLowerCase() == "y" || takesong.toLowerCase() == "e" || takesong.toLowerCase() == "k" || takesong.toLowerCase() == "h" || takesong.toLowerCase() == "u" || takesong.toLowerCase() == "d" || takesong.toLowerCase() == "a" || takesong.toLowerCase() == "murder 2" || takesong.toLowerCase() == "murder2" ){
+        if(takesong.toLowerCase() == "aye khuda"){
             document.getElementById("audio").innerHTML="<div id='song' id='play1'><table><tr><td><p id='titleimg' onclick='ayekhuda()'><img src='https://i.ytimg.com/vi/v0biEkJ9Uv8/sddefault.jpg' class='music'></p></td><td><p id='mtitle' onclick='ayekhuda()'>AYE KHUDA</p></td><td><p id='ayekhuda_button'></p></td><td onclick='d_ayekhuda()'><i class='fa fa-download' style='font-size:px;color:gold;'></i>DOWNLOAD</td></tr></table></div>";
         }
-        if(takesong.toLowerCase() == "teri meri prem kahani" || takesong.toLowerCase() == "t" || takesong.toLowerCase() == "e" || takesong.toLowerCase() == "r" || takesong.toLowerCase() == "i" || takesong.toLowerCase() == "m" || takesong.toLowerCase() == "p" || takesong.toLowerCase() == "k" || takesong.toLowerCase() == "a" || takesong.toLowerCase() == "h" || takesong.toLowerCase() == "n" || takesong.toLowerCase() == "bodyguard"){
+        if(takesong.toLowerCase() == "teri meri prem kahani"){
             document.getElementById("audio").innerHTML="<div id='song' id='terimeripremkahani'><table><tr><td><p id='titleimg' onclick='terimeripremkahani()'><img src='https://i1.sndcdn.com/artworks-000105449528-hgc39r-t500x500.jpg' class='music'></p></td><td><p id='mtitle' onclick='terimeripremkahani()'>TERI MERI PREM KAHANI</p></td><td><p id='terimeripremkahani_button'></p></td><td onclick='d_terimeripremkahani()'><i class='fa fa-download' style='font-size:px;color:gold;'></i>DOWNLOAD</td></tr></table></div>";
         }
         document.getElementById("mainhub").style.display="none";
         }
-        function ayekhuda(){
-            location.assign("/audio/ayekhuda.html");
+        if(takesong.toLowerCase() == "bodyguard"){
+            document.getElementById("audio").innerHTML="<div id='song' id='terimeripremkahani'><table><tr><td><p id='titleimg' onclick='terimeripremkahani()'><img src='https://i1.sndcdn.com/artworks-000105449528-hgc39r-t500x500.jpg' class='music'></p></td><td><p id='mtitle' onclick='terimeripremkahani()'>TERI MERI PREM KAHANI</p></td><td><p id='terimeripremkahani_button'></p></td></tr></table></div> <br>                                                                                                                       <div id='song'><table><tr><td><p id='titleimg' onclick='bodyguard_title()'><img src='https://i1.sndcdn.com/artworks-000105449528-hgc39r-t500x500.jpg' class='music'></p></td><td><p id='mtitle' onclick='bodyguard_title()'>BODYGUARD TITLE</p></td><td><p id='terimeripremkahan_button'></p></td></tr></table></div> <BR>                                                                                                                                                   <div id='song'><table><tr><td><p id='titleimg' onclick='bodyguard_iloveyou()'><img src='https://i1.sndcdn.com/artworks-000105449528-hgc39r-t500x500.jpg' class='music'></p></td><td><p id='mtitle' onclick='bodyguard_iloveyou()'>I LOVE YOU</p></td><td><p id='terimeripremkahan_button'></p></td></tr></table></div>";
         }
-        function terimeripremkahani(){
-            location.assign("/audio/terimeripremkahani.html");
+        document.getElementById("mainhub").style.display="none";
         }
-		function d_ayekhuda(){
-			location.assign("https://github.com/spark00001/spark/raw/refs/heads/main/Murder%202%20Aye%20Khuda%20Video%20With%20Lyrics%20Emraan%20Hashmi,%20Jacqueline%20Fernandez.mp3");
-		}
-		function d_terimeripremkahani(){
-			location.assign("https://github.com/spark00001/hindi-music/raw/refs/heads/main/Teri%20Meri.mp3");
-		}
-    
