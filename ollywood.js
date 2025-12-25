1bfd8936ee3597bca3fc929464a43d986d07881e
SPARK OFFICIAL <s20512165@gmail.com>
2025-12-25 20:42:59 +0530

    Update ollywood.js

--- ollywood.js
+++ ollywood.js
@@ -1,1 +1,4 @@
+function ollywood(){
+  const olly = setTimeout(ollywood,100); 
 document.getElementById("ollywood").innerHTML="<div id='gallery' class='gallery'>             <div class='image-item'>            <img src='/ollywood/kerek.png' alt='kerek' onclick='kerek();'> <p>KERE<BR>(2025)</p>        </div>                                   <div class='image-item'>            <img src='/ollywood/karma.png' alt='karma' onclick='karma();'> <p>KARMA<BR>(2024)</p>        </div>   </div>";
+}
