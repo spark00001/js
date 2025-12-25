f67f2a6e78b1edd4fa7053f6e1875df881f63db2
SPARK OFFICIAL <s20512165@gmail.com>
2025-12-25 21:34:08 +0530

    Update south.js

--- south.js
+++ south.js
@@ -1,6 +1,7 @@
 
-function south(){ 
+function southmovies(){ 
 document.getElementById("export").innerHTML="<div id='gallery' class='gallery'>    <!--2025-->	                                                                     <div class='image-item'>            <img src='/south/pechi.png' alt='jaat' onclick='pechi();' class='effect'> <p>PECHI <BR>(2024)</p>        </div>	                               <div class='image-item'>            <img src='/south/single.png' alt='single' onclick='single();' class='effect'> <p>SINGLE <BR>(2025)</p>        </div>	                                                    <div class='image-item'>            <img src='/south/kgf2.png' alt='KGF 1' onclick='kgf2();' class='effect'> <p>KGF 2<br>(2022)</p></div>                                                           <div class='image-item'>            <img src='/south/kgf1.png' alt='KGF 1' onclick='kgf1();' class='effect'> <p>KGF 1<br>(2018)</p>   </div> </div>";
 }
+
 
 
