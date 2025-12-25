11f7bf45a291b1f0bed9146ebade7e3b7345447b
SPARK OFFICIAL <s20512165@gmail.com>
2025-12-25 20:44:12 +0530

    Update pakdrama.js

--- pakdrama.js
+++ pakdrama.js
@@ -1,1 +1,4 @@
+const pak = setTimeout(pakdrama,100);
+function pakdrama(){ 
 document.getElementById("pakdrama").innerHTML="<div id='gallery' class='gallery'>  <div class='image-item'>            <img src='/pakdrama/kabhimainkabhitum.png' alt='kabhi main kabhi tum' onclick='kabhimainkabhitum();'> <p>KABHI MAIN <br> KABHI TUM<BR>(2024)</p>        </div>   </div>";
+}
