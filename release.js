ebbcaf97d1a1e2802b200117a99be36248bcb7a3
SPARK OFFICIAL <s20512165@gmail.com>
2025-12-26 17:34:57 +0530

    Create release.js

---
+++ release.js
@@ -0,0 +1,3 @@
+function upcomings(){ 
+document.getElementById("export").innerHTML="<div id='gallery' class='gallery'>  <div class='image-item'>            <img src='/ollywood/boubutubhuta.png' alt='bou butu bhuta' onclick='boubutubhuta();'> <p>BOU BUTU BHUTA<BR>(2025)</p>        </div>   </div>";
+}
