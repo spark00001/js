32358a2669cbed97b44b050fe5d544fd1aca53a9
SPARK OFFICIAL <s20512165@gmail.com>
2026-01-17 20:07:12 +0530

    Add documentary function to populate gallery

---
+++ documentary.js
@@ -0,0 +1,4 @@
+
+function documentary(){ 
+document.getElementById("export").innerHTML="<div id='gallery' class='gallery'>  <div class='image-item'>            <img src='/images/honeymoonsehatya.png' alt='honeymoon se hatya' onclick='honeymoonsehatya();'> <p>HONEYMOON SE <br>HATYA<BR>(2026)</p>        </div>   </div>";
+}
