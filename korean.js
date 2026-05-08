4ff17d463facfc9a37723823546cace15c91f78d
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-08 14:21:20 +0530

    Create korean.js

---
+++ korean.js
@@ -0,0 +1,4 @@
+
+function korean(){ 
+document.getElementById("export").innerHTML="<div id='gallery' class='gallery'>  <div class='image-item'>            <img src='/korean/whenlifegivesyoutangerines.png' alt='when life gives you tangerines' onclick='whenlifegivesyoutangerines();'> <p>WHEN LIFE<BR>GIVES YOU<BR>TANGERINES<BR>(2025)</p>        </div>   </div>";
+}
