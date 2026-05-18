813cff16772d69b2ac6e2527125a7d7f98deeab4
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-18 22:26:46 +0530

    Create sdb.js

---
+++ sdb.js
@@ -0,0 +1,10 @@
+function sdb(){
+var querry=document.getElementById("usrquerry").value;
+
+if(toLowercase.querry == "when life gives you tangeries"){
+location.assign("/spark/whenlifegivesyoutangeriesall.html");
+}
+else{
+document.getElementByid("throwout").innerHTML="not found";
+}
+}
