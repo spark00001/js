752f6444748da06c671147f0738ff9e96def4206
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-18 22:30:01 +0530

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,7 +1,6 @@
 document.getElementById("message").innerHTML="<table><tr><td><p id='msg'></p><p id='mess'></p></td><td>&nbsp;&nbsp;&nbsp;</td><td><p id='mail'></p></td><td>&nbsp;&nbsp;&nbsp;</td><td><p id='searchfinder'></p></td><td style='width:10%;'></td><td><p id='developer'></p></td></tr></table>";
 
 function finding(){
-location.assign("/spark/search.html");
 document.getElementById("msg").style.display="none";
 document.getElementById("mess").innerHTML="<p style='cursor:pointer;padding:10px; background-color:red;color:white; outline:3px solid white;cursor:pointer;width:50px;height:25px;' id='mess' onclick='off()'>CLOSE</p>";
 document.getElementById("mail").style.display="none";
