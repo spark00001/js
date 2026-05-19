e4762b0228c5ef08fb8280c81fe70c05d30dd516
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-19 10:19:54 +0530

    Update sdb.js

--- sdb.js
+++ sdb.js
@@ -1,8 +1,8 @@
 function sdb(){
 var querry=document.getElementById("usrquerry").value;
 
-if(querry.toLowerCase() == "when life gives you tangeries"){
-location.assign("/spark/whenlifegivesyoutangeriesall.html");
+if(querry.toLowerCase() == "when life gives you tangerines"){
+location.assign("/spark/whenlifegivesyoutangerines.html");
 }
 else{
 document.getElementById("throwout").innerHTML="not found";
