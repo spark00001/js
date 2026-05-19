9c160a44cf41278085f8504fc75bc7ced50cdae3
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-19 11:54:50 +0530

    Update sdb.js

--- sdb.js
+++ sdb.js
@@ -1,10 +1,61 @@
 function sdb(){
 var querry=document.getElementById("usrquerry").value;
 
+else{
+document.getElementById("throwout").innerHTML="<div id='result404'></div>";
+}
+
+
 if(querry.toLowerCase() == "when life gives you tangerines"){
 location.assign("/spark/whenlifegivesyoutangerines.html");
 }
-else{
-document.getElementById("throwout").innerHTML="not found";
+if(querry.toLowerCase() == "xo kitty season 3"){
+location.assign("/spark/xokittys3.html");
 }
+if(querry.toLowerCase() == "xo kitty season 2"){
+location.assign("/spark/xokittys2.html");
+}
+if(querry.toLowerCase() == "xo kitty season 1"){
+location.assign("/spark/xokittys1.html");
+}
+if(querry.toLowerCase() == "xo kitty"){
+location.assign("/spark/xokittyallseasons.html");
+}
+if(querry.toLowerCase() == "chiraiya season 1"){
+location.assign("/spark/chiraiyas1.html");
+}
+if(querry.toLowerCase() == "chiraiya"){
+location.assign("/spark/chiraiyall.html");
+}
+if(querry.toLowerCase() == "bhartha mahasayulaku wignyapthi"){
+location.assign("/spark/bharthamahasayulakuwignyapthi.html");
+}
+if(querry.toLowerCase() == "made in korea"){
+location.assign("/spark/madeinkorea.html");
+}
+if(querry.toLowerCase() == "srikanth"){
+location.assign("/spark/srikanth.html");
+}
+if(querry.toLowerCase() == "honeymoon se hatya"){
+location.assign("/spark/honeymoonsehatya.html");
+}
+if(querry.toLowerCase() == "bou butu bhuta"){
+location.assign("/spark/boubutubhuta.html");
+}
+if(querry.toLowerCase() == "surya the soldier"){
+location.assign("/spark/suryathesoldier.html");
+}
+if(querry.toLowerCase() == "geeta govinda"){
+location.assign("/spark/geetagovinda.html");
+}
+if(querry.toLowerCase() == "kerek"){
+location.assign("/spark/kerek.html");
+}
+if(querry.toLowerCase() == "karma"){
+location.assign("/spark/karma.html");
+}
+if(querry.toLowerCase() == "karma"){
+location.assign("/spark/karma.html");
+}
+
 }
