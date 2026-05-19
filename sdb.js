e2c81c6531d54965357f2ae1aa049632a96986a1
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-19 15:03:20 +0530

    Update sdb.js

--- sdb.js
+++ sdb.js
@@ -52,6 +52,99 @@ location.assign("/spark/karma.html");
 if(querry.toLowerCase() == "karma"){
 location.assign("/spark/karma.html");
 }
+if(querry.toLowerCase() == "kabhi main kabhi tum"){
+location.assign("/spark/kabhimainkabhitum.html");
+}
+if(querry.toLowerCase() == "vyarth"){
+location.assign("/spark/vyarth.html");
+}
+if(querry.toLowerCase() == "the broken table"){
+location.assign("/spark/thebrokentable.html");
+}
+if(querry.toLowerCase() == "illti"){
+location.assign("/spark/illti.html");
+}
+if(querry.toLowerCase() == "lekh"){
+location.assign("/spark/lekh.html");
+}
+if(querry.toLowerCase() == "sufna"){
+location.assign("/spark/sufna.html");
+}
+if(querry.toLowerCase() == "oye makhna"){
+location.assign("/spark/oye_makhna.html");
+}
+if(querry.toLowerCase() == "guddian patole"){
+location.assign("/spark/guddianpatole.html");
+}
+if(querry.toLowerCase() == "mitran da naa chalda"){
+location.assign("/spark/mitran_da_naa_chalda.html");
+}
+if(querry.toLowerCase() == "qismat"){
+location.assign("/spark/qismat.html");
+}
+if(querry.toLowerCase() == "qismat 2"){
+location.assign("/spark/qismat2.html");
+}
+if(querry.toLowerCase() == "head over heels"){
+location.assign("/spark/headoverheels.html");
+}
+if(querry.toLowerCase() == "live telecast"){
+location.assign("/spark/livetelecast.html");
+}
+if(querry.toLowerCase() == "campus diaries"){
+location.assign("/spark/campusdiaries.html");
+}
+if(querry.toLowerCase() == "mirzapur season 3"){
+location.assign("/spark/mirzapurs3.html");
+}
+if(querry.toLowerCase() == "mirzapur season 2"){
+location.assign("/spark/mirzapurs2.html");
+}
+if(querry.toLowerCase() == "mirzapur season 1"){
+location.assign("/spark/mirzapurs1.html");
+}
+if(querry.toLowerCase() == "mirzapur"){
+location.assign("/spark/mirzapur.html");
+}
+if(querry.toLowerCase() == "pechi"){
+location.assign("/spark/pechi.html");
+}
+if(querry.toLowerCase() == "single"){
+location.assign("/spark/single.html");
+}
+if(querry.toLowerCase() == "kgf chapter 1" || querry.toLowerCase() == "kgf 1"){
+location.assign("/spark/kgf1.html");
+}
+if(querry.toLowerCase() == "kgf chapter 2" || querry.toLowerCase() == "kgf 2"){
+location.assign("/spark/kgf2.html");
+}
+if(querry.toLowerCase() == "dhanak"){
+location.assign("/spark/dhanak.html");
+}
+if(querry.toLowerCase() == "the days of tafree"){
+location.assign("/spark/thedaysoftafree.html");
+}
+if(querry.toLowerCase() == "zora"){
+location.assign("/spark/zora.html");
+}
+if(querry.toLowerCase() == "jaat"){
+location.assign("/spark/jaat.html");
+}
+if(querry.toLowerCase() == "sikandar"){
+location.assign("/spark/sikandar.html");
+}
+if(querry.toLowerCase() == "autumn the black jaguar"){
+location.assign("/spark/autumntheblackjaguar.html");
+}
+if(querry.toLowerCase() == "minecraft"){
+location.assign("/spark/minecraft.html");
+}
+if(querry.toLowerCase() == "mocn knight"){
+location.assign("/spark/mocnknight.html");
+}
+if(querry.toLowerCase() == "halo"){
+location.assign("/spark/halo.html");
+}
 else{
 document.getElementById("throwout").innerHTML="<div id='result404'></div>";
 }
