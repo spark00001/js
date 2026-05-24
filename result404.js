dd40f4c59a130c80b73e15633baeb6ad85e23b99
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-24 18:15:48 +0530

    Create result404.js

---
+++ result404.js
@@ -0,0 +1,3 @@
+document.getElementById("result404").innerHTML=`
+<embed src="https://cdn.dribbble.com/userupload/22103334/file/original-f9edb621e79ac5717e6f6cce484f9201.gif" style="width:99%;height:auto;"></embed>
+`;
