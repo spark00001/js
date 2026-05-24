4957586a77fd3e221f8ce895617b305229961589
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-24 18:20:21 +0530

    Update result404.js

--- result404.js
+++ result404.js
@@ -1,3 +1,10 @@
 document.getElementById("result404").innerHTML=`
 <embed src="https://cdn.dribbble.com/userupload/22103334/file/original-f9edb621e79ac5717e6f6cce484f9201.gif" style="width:99%;height:auto;"></embed>
 `;
+function celarpage(){
+  document.getElementById("as").style.display="none";
+document.getElementById("upcoming").style.display="none";
+  document.getElementById("actors").style.display="none";
+  document.getElementById("catview").style.display="none";
+  document.getElementById("export").style.display="none";
+}
