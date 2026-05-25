397dd372db28115aec34cf2a7e995f13afbaf2c2
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-25 14:42:08 +0530

    Update chinese.js

--- chinese.js
+++ chinese.js
@@ -1,5 +1,5 @@
 
-function korean(){ 
+function chinese(){ 
 document.getElementById("export").innerHTML=`
 <div id='gallery' class='gallery'>  <!--<div class='image-item'>
 <img src='/korean/whenlifegivesyoutangerines.png' alt='when life gives you tangerines' onclick='whenlifegivesyoutangeriness1();'> <p>WHEN LIFE<BR>GIVES YOU<BR>TANGERINES<BR>(2025)</p>        </div>                 
