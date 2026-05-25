a2f44eab038c676fc8d4f7327882881f13c80b30
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-25 15:00:27 +0530

    Create genz.js

---
+++ genz.js
@@ -0,0 +1,6 @@
+const pullseasons = setTimeout(genz,100);
+function genz(){ 
+document.getElementById("export").innerHTML=`
+<div id='gallery' class='gallery'>                                                                                                                           <div class='image-item'>            <img src='/chinese/genz.png' alt='' onclick='genzs1();'> <p>GEN Z<BR>TANGERINES</BR><br>season 1</BR>(2023)</p>        </div></div>
+  `;
+}
