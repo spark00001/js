ded17344e5082ca7d72226493b3fbf964f34aada
SPARK OFFICIAL <s20512165@gmail.com>
2026-05-28 15:47:14 +0530

    Create hachi.js

---
+++ hachi.js
@@ -0,0 +1,7 @@
+const pullseasons = setTimeout(hachi
+,100);
+function hachi(){ 
+document.getElementById("export").innerHTML=`
+<div id='gallery' class='gallery'>                                                                                                                           <div class='image-item'>            <img src='/images/hachi.png' alt='' onclick='hachi();'> <p>HACHI<br>(2009)</br></p>        </div></div>
+  `;
+}
