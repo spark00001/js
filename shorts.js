2b07c371ef24b8361f06ce03fb1b4bc0ded73259
smd <smarakd64@gmail.com>
2026-07-03 02:28:06 +0530

    image path updated

--- shorts.js
+++ shorts.js
@@ -2,7 +2,7 @@ function shorts(){
     document.getElementById("export").innerHTML=`
 <div id='gallery' class='gallery'> 
 <div class='image-item'>            
-<img src='/shorts_poster/kingskillercomeback.png' alt='kings killer comeback' onclick='kingskillercomebacks1()'> 
+<img src='/images/kingskillercomeback.png' alt='kings killer comeback' onclick='kingskillercomebacks1()'> 
 <p>KINGS KILLER COMEBACK<BR>(2026)</p>        
 </div>  
 </div>
