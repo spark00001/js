e0b424d74dacc91bb9ea95ddfdc6602cc36cceef
smd <smarakd64@gmail.com>
2026-07-03 02:15:07 +0530

    SHORTS UPDATED

--- shorts.js
+++ shorts.js
@@ -1,3 +1,10 @@
 function shorts(){
-location.assign("/spark/shorts.html");
+    document.getElementById("export").innerHTML=`
+<div id='gallery' class='gallery'> 
+<div class='image-item'>            
+<img src='/shorts_poster/kingskillercomeback.png' alt='kings killer comeback' onclick='kingskillercomebacks1()'> 
+<p>KINGS KILLER COMEBACK<BR>(2026)</p>        
+</div>  
+</div>
+`;
 }
