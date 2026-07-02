1ad07e1ec88b447b503b13ab338d71a0948638bb
smd <smarakd64@gmail.com>
2026-07-03 01:44:45 +0530

    korean structure updated

--- korean.js
+++ korean.js
@@ -1,4 +1,23 @@
 
 function korean(){ 
-document.getElementById("export").innerHTML="<div id='gallery' class='gallery'>  <div class='image-item'>            <img src='/korean/whenlifegivesyoutangerines.png' alt='when life gives you tangerines' onclick='whenlifegivesyoutangeriness1();'> <p>WHEN LIFE<BR>GIVES YOU<BR>TANGERINES<BR>(2025)</p>        </div>                 <div class='image-item'>            <img src='/images/xokittys3.png' alt='xo kitty season 3' onclick='xokittys3();'> <p>XO KITTY<BR>SEASON 3<BR>(2023)</p>        </div>                        <div class='image-item'>            <img src='/images/xokittys2.png' alt='xo kitty season 2' onclick='xokittys2();'> <p>XO KITTY<BR>SEASON 2<BR>(2023)</p>        </div>           <div class='image-item'>            <img src='/images/xokittys1.png' alt='xo kitty season 1' onclick='xokittys1();'> <p>XO KITTY<BR>SEASON 1<BR>(2023)</p>        </div>  </div>";
+document.getElementById("export").innerHTML=`
+<div id='gallery' class='gallery'>  
+<div class='image-item'>            
+<img src='/korean/whenlifegivesyoutangerines.png' alt='when life gives you tangerines' onclick='whenlifegivesyoutangeriness1();'> 
+<p>WHEN LIFE<BR>GIVES YOU<BR>TANGERINES<BR>(2025)</p>        
+</div>                 
+<div class='image-item'>            
+<img src='/images/xokittys3.png' alt='xo kitty season 3' onclick='xokittys3();'> 
+<p>XO KITTY<BR>SEASON 3<BR>(2023)</p>        
+</div>                        
+<div class='image-item'>            
+<img src='/images/xokittys2.png' alt='xo kitty season 2' onclick='xokittys2();'> 
+<p>XO KITTY<BR>SEASON 2<BR>(2023)</p>        
+</div>           
+<div class='image-item'>            
+<img src='/images/xokittys1.png' alt='xo kitty season 1' onclick='xokittys1();'> 
+<p>XO KITTY<BR>SEASON 1<BR>(2023)</p>        
+</div>  
+</div>
+`;
 }
