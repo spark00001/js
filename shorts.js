4dd81cedeb09772d9d23528a0b0ee77f37388608
SPARK OFFICIAL <s20512165@gmail.com>
2026-07-03 13:01:36 +0530

    update

--- shorts.js
+++ shorts.js
@@ -2,7 +2,7 @@ function shorts(){
     document.getElementById("export").innerHTML=`
 <div id='gallery' class='gallery'> 
 <div class='image-item'>            
-<img src='/images/kingskillercomeback.png' alt='kings killer comeback' onclick='kingskillercomeback()'> 
+<img src='/images/kingskillercomeback.png' alt='kings killer comeback' onclick='kingskillercomeback();'> 
 <p>KINGS KILLER<br>COMEBACK</p>        
 </div>  
 </div>`;
