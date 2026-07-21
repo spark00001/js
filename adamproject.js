7b2bf2dd5affa54f038d9bead7e6081c62d42c45
SPARK OFFICIAL <s20512165@gmail.com>
2026-07-21 22:29:40 +0530

    Create adamproject.js

---
+++ adamproject.js
@@ -0,0 +1,6 @@
+document.getElementById("adamproject").innerHTML=`
+<div class='image-item'>            
+        <img src='/images/movie-3.png' onclick='adam_project();'>            
+        <p>ADAM PROJECT<br>(2022)</p>        
+      </div>
+`;
