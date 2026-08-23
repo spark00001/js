ac0a7217442a7820c2a7dc0e2a207f37680c4a24
SPARK OFFICIAL <s20512165@gmail.com>
2026-08-23 23:21:15 +0530

    Update shorts.js

--- shorts.js
+++ shorts.js
@@ -2,6 +2,10 @@ function shorts(){
     document.getElementById("export").innerHTML=`
 <div id='gallery' class='gallery'> 
 <div class='image-item'>            
+<img src='/images/chaalbazhacker.png' alt='chaalbaz hacker' onclick='chaalbazhacker();'> 
+<p>CHAALBAZ<BR>HACKER</p>        
+</div>  
+<div class='image-item'>            
 <img src='/images/kingskillercomeback.png' alt='kings killer comeback' onclick='kingskillercomeback();'> 
 <p>KINGS KILLER<br>COMEBACK</p>        
 </div>  
