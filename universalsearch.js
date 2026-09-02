34d714a35c7d4d2f8650ebb6a80680a4467add41
SPARK OFFICIAL <s20512165@gmail.com>
2026-09-02 21:13:17 +0530

    Update universalsearch.js

--- universalsearch.js
+++ universalsearch.js
@@ -5,7 +5,7 @@ function universalsearch(){
 return `
 <form class="auto-search-wrapper"">
   <i class="fa-solid fa-magnifying-glass auto-search-icon"></i>
-  <input type="text" id="autoSearchInput" class="auto-search-input" placeholder="Search..." autocomplete="on" required/>
+  <input type="text" id="usrsearch" class="auto-search-input" placeholder="Search..." autocomplete="on" required/>
   <button type="button" onclick="searchdb()" class="auto-search-btn" aria-label="Search">
     <span class="auto-search-btn-text">Search</span>
     <i class="fa-solid fa-arrow-right"></i>
