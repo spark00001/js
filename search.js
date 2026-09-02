14328e388f47d8a18d5e67e55d85fed4466e2d11
SPARK OFFICIAL <s20512165@gmail.com>
2026-09-02 10:40:33 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,14 +1,14 @@
+var reloadhome = livetvview();
 var sysoutput = "";
 function searchdb(){
 var search = document.getElementById("usrsearch").value;
 switch(search.toLowerCase().trim()){
 case "adam project":
 sysoutput = adamprojectjs();
-document.getElementById("exportsearch").innerHTML=sysoutput;
-document.getElementById("export").style.display="none";
+document.getElementById("export").innerHTML=sysoutput;
 break;
 default:
- livetvview();
+ document.getElementById("export").innerHTML=reloadhome;
 break;
 }
 }
