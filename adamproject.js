63c15124ff8e721bb081e7a0774459f41ef22c4b
SPARK OFFICIAL <s20512165@gmail.com>
2026-09-02 19:02:33 +0530

    Update adamproject.js

--- adamproject.js
+++ adamproject.js
@@ -1,8 +1,10 @@
 function adamprojectjs(){
 return `
-<div class='image-item'>            
+<div class='image-item'>
+<div class='image-item'>
         <img src='/images/movie-3.png' onclick='adam_project();'>            
         <p>ADAM PROJECT<br>(2022)</p>        
+      </div>
       </div>
 `;
 }
